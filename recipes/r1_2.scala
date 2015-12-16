9f9fa661a187961be4b37923580bf16f8cadf7b7
wonghoifung <wonghoifung85@gmail.com>
2015-12-16 22:16:28 +0800

    multi line

---
+++ recipes/r1_2.scala
@@ -0,0 +1,17 @@
+val mlt="""abbbbbbbbbbb
+	|zzzzzzzzzzzzzzzzzc
+	hello
+	|worlda""".stripMargin
+
+println(mlt)
+
+println(mlt.replaceAll("\n"," "))
+
+val text = """11111111
+123123123
+456456456
+"fuck"
+'damn'
+789789"""
+
+println(text)

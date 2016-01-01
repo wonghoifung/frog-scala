d154d0bc205c3764acdad7b9bd1773f26fef0010
wonghoifung <wonghoifung85@gmail.com>
2016-01-01 22:38:59 +0800

    big number

---
+++ recipes/r2_6.scala
@@ -0,0 +1,22 @@
+var b=BigInt(1234567890)
+
+println(b)
+
+var bb = BigDecimal(123456.789)
+
+println(bb)
+
+println(b + b)
+
+println(b * b)
+
+println(b.toInt)
+println(b.toLong)
+println(b.toFloat)
+
+println(b.isValidInt)
+
+println(Int.MaxValue)
+
+println(Double.PositiveInfinity)
+println(Double.NegativeInfinity)

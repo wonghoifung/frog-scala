5d40fc7fb8b05e0bc4e7584c82f33e01683b847b
wonghoifung <wonghoifung85@gmail.com>
2016-01-11 03:26:54 -0500

    forcing callers to leave parentheses off accessor methods

---
+++ recipes/r5_6.scala
@@ -0,0 +1,13 @@
+class Pizza {
+  def crustSize() = 12
+  def crustSize2 = 24
+}
+
+val p = new Pizza
+
+println(p.crustSize())
+println(p.crustSize)
+
+//println(p.crustSize2())
+println(p.crustSize2)
+
